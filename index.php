98cc4e69b53360d6ae8b1b63277127020b06c1ec
Farid Adam <faridibin@gmail.com>
2018-06-20 18:54:51 +0000

    Update

--- index.php
+++ index.php
@@ -3,7 +3,15 @@
 <html lang="en">
     <?php App::ViewPartial('header', 'html_blocks')?>
     <body class=" ">
-        <?php require_once 'config/Routes/Routes.php'; ?>
+        <a id="start"></a>
+        <?php App::ViewPartial('side-bar', 'html_blocks')?>
+        <div class="main-container">
+            <?php require_once 'config/Routes/Routes.php'; ?>
+            <?php App::ViewPartial('footer', 'html_blocks')?>
+        </div>
+        <a class="back-to-top inner-link" href="#start" data-scroll-class="100vh:active">
+            <i class="stack-interface stack-up-open-big"></i>
+        </a>
         <?php App::ViewPartial('js-scripts', 'html_blocks')?>
     </body>
 </html>
