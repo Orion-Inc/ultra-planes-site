ecd3745aa7efd7107d8d1218738b86345a613f28
Farid Adam <faridibin@gmail.com>
2018-06-22 18:26:34 +0000

    Update

--- resources/partials/forms/contact-form.php
+++ resources/partials/forms/contact-form.php
@@ -1,4 +1,4 @@
-<form class="form-email" data-success="Thanks for your message, we'll be in touch shortly." data-error="Please make sure all required fields have been filled.">
+<form class="form-email" action="config/actions/send-mail.php" data-success="Thanks for your message, we'll be in touch shortly." data-error="Please make sure all required fields have been filled.">
     <div class="row">
         <div class="col-md-6">
             <span>Your Full Name</span>
