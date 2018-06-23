2752789a830551ab5a64a7ca39d320f3484228eb
Adetunji Salako <adetunjisalako@gmail.com>
2018-06-23 14:44:41 +0000

    Update

--- resources/views/privacy-policy.php
+++ resources/views/privacy-policy.php
@@ -7,6 +7,109 @@
                     <li>This Application collects some Personal Data from its Users.</li>
                 </ol>
                 <hr>
+                <!--Beginning of summary policy-->
+                <div class = "orionP-summary">
+                    <div class="col-md-12">
+                        <h2>Policy summary</h2>
+                    </div>
+                    <hr>
+                    <h4 id="purposes_data">Personal Data collected for the following purposes and using the following services:</h2>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class = "row">
+                                <div class="col-md-4">
+                                    <i class="fab fa-adversal fa-5x"></i>
+                                </div>
+                                <div class="col-md-8">
+                                    <h4>Advertising</h4>
+                                    <ul class="unstyled">
+                                        <li>
+                                            <h6>Unity Ads</h6>
+                                            <p>Personal Data: Cookies and Usage Data</p>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class = "row">
+                                <div class="col-md-4">
+                                <i class="fas fa-warehouse fa-5x"></i>
+                                </div>
+                                <div class="col-md-8">
+                                    <h4>Hosting and backend infrastructure</h4>
+                                    <ul class="unstyled">
+                                        <li>
+                                            <h6>Google Cloud Storage</h6>
+                                            <p>Personal Data: various types of Data as specified in the privacy policy of the service</p>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class = "row">
+                                <div class="col-md-4">
+                                    <i class="fas fa-chart-area fa-5x"></i>
+                                </div>
+                                <div class="col-md-8">
+                                    <h4>Analytics</h4>
+                                    <ul class="unstyled">
+                                        <li>
+                                            <h6>Google Analytics</h6>
+                                            <p>Personal Data: Cookies and Usage Data</p>
+                                        </li>
+                                        <li>
+                                            <h6>Unity Analytics</h6>
+                                            <p>Personal Data: Usage Data and various types of Data as specified in the privacy policy of the service</p>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class ="col-md-12" >
+                            <div>
+                                <h2 id="contact_information">Contact information</h2>
+                            </div>
+                        <div class="col-md-6">
+                            <div class = "row">
+                                <div class="col-md-4">
+                                    <i class="fas fa-chart-area fa-5x"></i>
+                                </div>
+                                <div class="col-md-8">
+                                    <h4>Owner and Data Controller</h4>
+                                    <h6>Orion GH</h6>
+                                    <ul class="unstyled">
+                                        <li> 
+                                            <p>Valley view university, Oyibi, Accra, Ghana</p>
+                                            <p>Owner contact email: info@orionic.tech</p>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+                <!--end of summary policy-->
+                <hr>
+                <div class="col-md-12">
+                        <h2>Full policy</h2>
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="col-md-6">
+                        <h4>Owner and Data Controller</h4>
+                        <h6>Orion GH</h6>
+                        <ul class="unstyled">
+                            <li> 
+                                <p>Valley view university, Oyibi, Accra, Ghana</p>
+                                <p>Owner contact email: info@orionic.tech</p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                </div>
             </div>
         </div>
         <!--end of row-->
