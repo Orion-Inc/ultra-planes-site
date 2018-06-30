68a629240d5de2bb584055d6b3921bd948ad6060
Farid Adam <faridibin@gmail.com>
2018-06-30 18:08:38 +0000

    Update

--- resources/views/home.php
+++ resources/views/home.php
@@ -1,30 +1,17 @@
-<section class="cover height-60 hidden">
+<section class="cover height-60">
     <div class="container pos-vertical-center">
         <div class="row">
             <div class="col-md-11 col-lg-8">
                 <span class="h2">
-                    <span class="color--primary">Hi, we're Stack</span> a digital design firm specialising in end-to-end marketing campaigns. We partner with startups and companies, small and large.
+                    <span class="color--primary">Lorem ipsum dolor sit,</span> amet consectetur adipisicing elit. Asperiores deserunt id obcaecati a, soluta et itaque perspiciatis necessitatibus.
                 </span>
-                <div class="modal-instance block">
-                    <div class="video-play-icon video-play-icon--sm bg--primary modal-trigger"></div>
-                    <span>
-                        <strong>Watch our film</strong>&nbsp;&nbsp;&nbsp;104 Seconds</span>
-                    <div class="modal-container">
-                        <div class="modal-content bg-dark" data-width="60%" data-height="60%">
-                            <iframe data-src="https://www.youtube.com/embed/6p45ooZOOPo?autoplay=1" allowfullscreen="allowfullscreen"></iframe>
-                        </div>
-                        <!--end of modal-content-->
-                    </div>
-                    <!--end of modal-container-->
-                </div>
-                <!--end of modal instance-->
             </div>
         </div>
         <!--end of row-->
     </div>
     <!--end of container-->
 </section>
-<section>
+<section class="hidden">
     <div class="container">
         <div class="row">
             <div class="slider slider--inline-arrows slider--arrows-hover" data-arrows="true">
@@ -54,115 +41,54 @@
     </div>
     <!--end of container-->
 </section>
-<section class="bg--secondary hidden">
+<section class="bg--dark">
     <div class="container">
         <div class="row">
             <div class="col-md-4">
                 <div class="feature feature-1">
-                    <img alt="Image" src="img/cowork-8.jpg" />
                     <div class="feature__body boxed boxed--lg boxed--border">
-                        <h3>Stack Coworking</h3>
+                        <h3>Feature</h3>
                         <p class="lead">
-                            Save time with a multitude of styled components designed to showcase your content
+                            Description
                         </p>
                         <a href="#">
-                            Explore Project &rarr;
+                            Explore Feature &rarr;
                         </a>
                     </div>
                 </div>
-                <!--end feature-->
             </div>
             <div class="col-md-4">
                 <div class="feature feature-1">
-                    <img alt="Image" src="img/work-6.jpg" />
                     <div class="feature__body boxed boxed--lg boxed--border">
-                        <h3>Nike Active</h3>
+                        <h3>Feature</h3>
                         <p class="lead">
-                            Save time with a multitude of styled components designed to showcase your content
+                            Description
                         </p>
                         <a href="#">
-                            Explore Project &rarr;
+                            Explore Feature &rarr;
                         </a>
                     </div>
                 </div>
-                <!--end feature-->
             </div>
             <div class="col-md-4">
                 <div class="feature feature-1">
-                    <img alt="Image" src="img/work-3.jpg" />
                     <div class="feature__body boxed boxed--lg boxed--border">
-                        <h3>M&amp;S Stairs Co.</h3>
+                        <h3>Feature</h3>
                         <p class="lead">
-                            Save time with a multitude of styled components designed to showcase your content
+                            Description
                         </p>
                         <a href="#">
-                            Explore Project &rarr;
+                            Explore Feature &rarr;
                         </a>
                     </div>
                 </div>
-                <!--end feature-->
             </div>
         </div>
-        <!--end of row-->
     </div>
-    <!--end of container-->
 </section>
-<section class=" bg--secondary hidden">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="slider slider--inline-arrows" data-arrows="true">
-                    <ul class="slides">
-                        <li>
-                            <div class="testimonial row justify-content-center">
-                                <div class="col-lg-2 col-md-4 col-6 text-center">
-                                    <img class="testimonial__image" alt="Image" src="img/avatar-round-1.png" />
-                                </div>
-                                <div class="col-lg-7 col-md-8 col-12">
-                                    <span class="h3">&ldquo;We’ve been using Stack to prototype designs quickly and efficiently. Needless to say we’re hugely impressed by the style and value.&rdquo;
-                                    </span>
-                                    <h5>Maguerite Holland</h5>
-                                    <span>Interface Designer &mdash; Yoke</span>
-                                </div>
-                            </div>
-                        </li>
-                        <li>
-                            <div class="testimonial row justify-content-center">
-                                <div class="col-lg-2 col-md-4 col-6 text-center">
-                                    <img class="testimonial__image" alt="Image" src="img/avatar-round-4.png" />
-                                </div>
-                                <div class="col-lg-7 col-md-8 col-12">
-                                    <span class="h3">&ldquo;I've been using Medium Rare's templates for a couple of years now and Stack is without a doubt their best work yet. It's fast, performant and absolutely stunning.&rdquo;
-                                    </span>
-                                    <h5>Lucas Nguyen</h5>
-                                    <span>Freelance Designer</span>
-                                </div>
-                            </div>
-                        </li>
-                        <li>
-                            <div class="testimonial row justify-content-center">
-                                <div class="col-lg-2 col-md-4 col-6 text-center">
-                                    <img class="testimonial__image" alt="Image" src="img/avatar-round-3.png" />
-                                </div>
-                                <div class="col-lg-7 col-md-8 col-12">
-                                    <span class="h3">&ldquo;Variant has been a massive plus for my workflow &mdash; I can now get live mockups out in a matter of hours, my clients really love it.&rdquo;
-                                    </span>
-                                    <h5>Rob Vasquez</h5>
-                                    <span>Interface Designer &mdash; Yoke</span>
-                                </div>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-        <!--end of row-->
-    </div>
-    <!--end of container-->
-</section>
-<section class="video video-1 text-center imagebg space--lg hidden" data-overlay="3">
+<section class="video video-1 text-center imagebg space--lg" data-overlay="3">
     <div class="background-image-holder">
-        <img alt="background" src="img/cowork-1.jpg" />
+        <img alt="background" src="<?php App::asset('bg-trailer.png','img/backgrounds')?>"/>
     </div>
     <div class="container">
         <div class="row">
@@ -171,14 +97,11 @@
                     <div class="video-play-icon video-play-icon--sm modal-trigger box-shadow"></div>
                     <div class="modal-container">
                         <div class="modal-content bg-dark" data-width="60%" data-height="60%">
-                            <iframe data-src="https://www.youtube.com/embed/6p45ooZOOPo?autoplay=1" allowfullscreen="allowfullscreen"></iframe>
+                            <iframe data-src="https://www.youtube.com/embed/YPtjcT1t64I?autoplay=1" allowfullscreen="allowfullscreen"></iframe>
                         </div>
-                        <!--end of modal-content-->
                     </div>
-                    <!--end of modal-container-->
                 </div>
-                <!--end of modal instance-->
-                <h2>Watch our Process film</h2>
+                <h2>Watch Trailer</h2>
             </div>
         </div>
         <!--end of row-->
